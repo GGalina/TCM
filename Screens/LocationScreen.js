8408624e577401d5e57bc554ea58efdafba28bd3
GGalina <71042425+GGalina@users.noreply.github.com>
2024-03-15 23:25:30 +0400

    ADD: Location screen

--- Screens/LocationScreen.js
+++ Screens/LocationScreen.js
@@ -1,38 +1,64 @@
-import React from 'react';
-import { View, Image, StyleSheet } from 'react-native';
-import Infinity from '../assets/img/Infinity.gif';
-import Gif from 'react-native-gif';
+import MapView, { Marker } from "react-native-maps";
+import { Dimensions, StyleSheet, View} from 'react-native';
 
 export const LocationScreen = () => {
-  return (
-    <View style={styles.container}>
-      <Image
-        source={require('../assets/img/logo.jpg')}
-        style={styles.logo}
-        />
 
-           <Gif
-        source={Infinity} // Path to your animated GIF file
-        style={styles.gif}
-      />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <MapView
+                style={styles.map}
+                region={{
+                    latitude: 25.2048,
+                    longitude: 55.2708,
+                    latitudeDelta: 0.3, 
+                    longitudeDelta: 0.3,
+                }}
+                mapType="standard"
+            >
+                <Marker
+                    title={'TCM Acupuncture Therapy Center( Branch)'}
+                    description={'Working Hours: 9:00 - 00:00'}
+                    coordinate={{ latitude: 25.11616, longitude: 55.20278 }}
+                /> 
+                <Marker
+                    title={'TCM Acupuncture and Ayurveda Therapy Center Branch-Shop 8'}
+                    description={'Working Hours: 9:00 - 00:00'}
+                    coordinate={{ latitude: 25.11771, longitude: 55.19528 }}
+                /> 
+                <Marker
+                    title={'Al Barsha Therapy Center'}
+                    description={'Working Hours: 9:00 - 00:00'}
+                    coordinate={{ latitude: 25.11623, longitude: 55.19575 }}
+                /> 
+                <Marker
+                    title={'TCM Acupuncture Therapy Center -Shop 5'}
+                    description={'Working Hours: 9:00 - 23:00'}
+                    coordinate={{ latitude: 25.11646, longitude: 55.19484 }}
+                />
+                <Marker
+                    title={'TCM Acupuncture Center-Jumeirah'}
+                    description={'Working Hours: 10:00 - 23:30'}
+                    coordinate={{ latitude: 25.14337, longitude: 55.19106 }}
+                />
+                <Marker
+                    title={'TCM Massage & Relaxation Center Bay Square'}
+                    description={'Working Hours: 9:00 - 00:00'}
+                    coordinate={{ latitude: 25.18920, longitude: 55.28050 }}
+                />
+            </MapView>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  logo: {
-    width: 150, 
-    height: 150, 
-    resizeMode: 'contain', 
-    marginBottom: 20, 
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
     },
-    gif: {
-        width: 50,
-        height: 50,
-  }
+    map: {
+        width: Dimensions.get("window").width,
+        height: Dimensions.get("window").height,
+    },
 });
